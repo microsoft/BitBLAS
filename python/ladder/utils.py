6371742bf9314f24da683468e2f6b8c3fa16d847
LeiWang <leiwang1999@outlook.com>
2024-01-08 06:21:58 -0400

    extend log print

--- python/ladder/utils.py
+++ python/ladder/utils.py
@@ -342,3 +342,29 @@ def compile_parallel(cpresults, arch, timeout: float = None):
             [timeout for _ in cpresults],
         )
     return list(libs)
+
+
+count = 0
+
+def init_count():
+    global count
+    count = 0
+    return count
+
+def write_code(code, path, fname):
+    global count
+    # if path not exist, create it
+    fname = str(count) + "." + fname
+    count += 1
+    if not os.path.exists(path):
+        os.makedirs(path)
+    # join path and fname
+    fname = os.path.join(path, fname)
+    with open(fname, "w") as f:
+        f.write(code)
+
+def write_mod(mod, path, fname):
+    py_fname = fname + ".py"
+    write_code(mod.script(show_meta=False), path, py_fname)
+    cu_fname = fname + ".cu"
+    write_code(mod.astext(show_meta_data=False), path, cu_fname)
