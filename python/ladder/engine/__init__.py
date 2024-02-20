e0169189bedc18ff71f1b0b2913d62ac1ed0f2ac
LeiWang <leiwang1999@outlook.com>
2024-02-20 02:42:41 -0400

    savemodel

--- python/ladder/engine/__init__.py
+++ python/ladder/engine/__init__.py
@@ -1,5 +1,5 @@
 from .base_tunner import Tunner
-from .common import save_results, export_groups
+from .common import save_models, save_results, export_groups
 from .engine import Engine
 from .load_model import load_model
 from .multiproc_tunner import MultiProcTunner
