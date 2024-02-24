fe923c36aa2ad6c3511e29f37c25d3d9297e3b7c
LeiWang <leiwang1999@outlook.com>
2024-02-24 09:22:27 -0400

    add storage dtype test

--- testing/python/operators/test_ladder_permutate_ops.py
+++ testing/python/operators/test_ladder_permutate_ops.py
@@ -8,18 +8,19 @@ from bitblas.ops.ladder_permutate import LadderPermutate, LadderPermutateConfig
 target = tvm.target.Target("llvm")
 
 # fmt: off
-@pytest.mark.parametrize("M,N,datatype,dequantize_bits,propagate_kind,transpose_matrix,transform_kind,target_instruction", [
-    (1024, 1024, "float16", -1, "B", True, 0, "nvidia-mma"),
-    (1024, 1024, "float16", -1, "B", True, 1, "nvidia-mma"),
-    (1024, 1024, "float16", -1, "B", True, 2, "nvidia-mma"),
+@pytest.mark.parametrize("M,N,datatype,dequantize_bits,storage_dtype,propagate_kind,transpose_matrix,transform_kind,target_instruction", [
+    (1024, 1024, "float16", -1, "float16", "B", True, 0, "nvidia-mma"),
+    (1024, 1024, "float16", -1, "float16", "B", True, 1, "nvidia-mma"),
+    (1024, 1024, "float16", -1, "float16", "B", True, 2, "nvidia-mma"),
     # dequantize propagation
-    (1024, 1024, "float16", 4, "B", True, 2, "nvidia-mma"),
+    (1024, 1024, "float16", 4, "uint32", "B", True, 2, "nvidia-mma"),
 ])
 def test_ladder_permutate_profile_latency(
     M,
     N,
     datatype,
     dequantize_bits,
+    storage_dtype,
     propagate_kind,
     transpose_matrix,
     transform_kind,
@@ -31,6 +32,7 @@ def test_ladder_permutate_profile_latency(
         N=N,
         datatype=datatype,
         dequantize_bits=dequantize_bits,
+        storage_dtype=storage_dtype,
         propagate_kind=propagate_kind,
         transpose_matrix=transpose_matrix,
         transform_kind=transform_kind,
