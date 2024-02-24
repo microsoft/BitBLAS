ac14d25ae9b23673bbae9fc53559be81ad19a470
LeiWang <leiwang1999@outlook.com>
2024-02-24 05:24:26 -0400

    init logger

--- python/bitblas/__init__.py
+++ python/bitblas/__init__.py
@@ -14,3 +14,42 @@ from .base import (
     try_inline,
     try_inline_contiguous_spatial,
 )
+
+from .relax import transform
+from . import testing
+
+import logging
+from tqdm import tqdm
+
+
+# target logger into tqdm.write
+class TqdmLoggingHandler(logging.Handler):
+    def __init__(self, level=logging.NOTSET):
+        super().__init__(level)
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            tqdm.write(msg)
+        except Exception:
+            self.handleError(record)
+
+
+def set_log_level(level):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level)
+
+
+def _init_logger():
+    logger = logging.getLogger(__name__)
+    handler = TqdmLoggingHandler()
+    formatter = logging.Formatter(
+        fmt="%(asctime)s [BitBLAS:%(levelname)s]: %(message)s", datefmt="%F %T"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.propagate = False
+    set_log_level(logging.INFO)
+
+
+_init_logger()
