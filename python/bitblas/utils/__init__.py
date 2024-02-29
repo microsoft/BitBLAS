59b96225fd0a5924f70f36e4be9bb50fa5033dde
LeiWang1999 <wanglei21c@mails.ucas.ac.cn>
2024-02-29 02:31:23 +0000

    optimize target

--- python/bitblas/utils/__init__.py
+++ python/bitblas/utils/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 from .tensor_adapter import tvm_tensor_to_torch
+import os
 import re
 
 def match_global_kernel(source: str) -> int:
@@ -8,3 +9,6 @@ def match_global_kernel(source: str) -> int:
     matched = re.findall(pattern, source)
     assert len(matched) > 1  # may have statement before kernel
     return source.index(matched[0])
+
+def get_target_from_env() -> str:
+    return os.environ.get("TVM_TARGET") or "cuda"
