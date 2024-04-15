b329110cde18ebd2cbde3ec725a25ad4210f3393
LeiWang <leiwang1999@outlook.com>
2024-04-15 10:29:15 -0400

    Implement BitNET LOP3 Test

--- testing/cpp/lop3_type_conversion/lowprecision_to_float16.cu
+++ testing/cpp/lop3_type_conversion/lowprecision_to_float16.cu
@@ -300,7 +300,7 @@ TEST(DecodeTest, DecodeInt1ToFloat16)
     cudaCheckLastError(cudaFree(decoded_gpu));
     for (int i = 0; i < N; i++)
     {
-        EXPECT_EQ(in_data[i], int(decoded[i]));
+        EXPECT_EQ(2 * in_data[i] - 1, int(decoded[i]));
     }
     free(ins);
     free(interleaved);
