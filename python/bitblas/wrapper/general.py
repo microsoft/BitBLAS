c9f0b7a462c505d7b7fe81fc050d7baae8674bb0
Lei Wang <34334180+LeiWang1999@users.noreply.github.com>
2024-04-24 01:51:21 +0800

    [CUDA GRAPH] Support Cuda Stream in the Wrap Function (#21)
    * issue fix from loading database.
    
    * support cuda stream
    
    * Refactor CUDA kernel launch string formatting in CUDASourceWrapper class

--- python/bitblas/wrapper/general.py
+++ python/bitblas/wrapper/general.py
@@ -247,6 +247,7 @@ class CUDASourceWrapper(object):
         for dyn_sym in dynamic_symbolic_set:
             function_args.append({"name": dyn_sym, "type": "int"})
 
+        function_args.append({"name": "stream=0", "type": "cudaStream_t"},)
         # Format the function arguments for declaration
         def_args = ", ".join([f"{arg['type']} {arg['name']}" for arg in function_args])
 
@@ -284,8 +285,8 @@ class CUDASourceWrapper(object):
         # Determine the shared memory size, defaulting to 0 if not specified
         smem_str = 0 if self.dynamic_smem_buf is None else self.dynamic_smem_buf
         # Format the CUDA kernel launch string
-        call_str = "{}<<<{}, {}, {}>>>({});".format(function_name, grid_str, block_str, smem_str,
-                                                    call_args)
+        call_str = "{}<<<{}, {}, {}, stream>>>({});".format(function_name, grid_str, block_str,
+                                                            smem_str, call_args)
         # Create the host function wrapper for the CUDA kernel
         host_func = """
     extern "C" void call({}) {{
@@ -351,6 +352,8 @@ extern "C" void init() {{
         for dyn_sym in dynamic_symbolic_set:
             function_args.append({"name": dyn_sym, "type": "int"})
 
+        function_args.append({"name": "stream=0", "type": "cudaStream_t"},)
+
         # Format the argument definitions for function declaration
         def_args = ", ".join([f"{arg['type']} {arg['name']}" for arg in function_args])
 
@@ -401,7 +404,7 @@ extern "C" void init() {{
             (symbolic,) = list(dynamic_symbolic_set)
             range_str = opt_shapes[symbolic]
             if last_range == 0:
-                call_str = "if ({} <= {}) {{\n\t\t\t {}<<<{}, {}, {}>>>({}); \n\t\t}}\n".format(
+                call_str = "if ({} <= {}) {{\n\t\t\t {}<<<{}, {}, {}, stream>>>({}); \n\t\t}}\n".format(
                     symbolic,
                     range_str,
                     function_name,
@@ -411,7 +414,7 @@ extern "C" void init() {{
                     call_args,
                 )
             else:
-                call_str = "\t\telse if ({} <= {}) {{\n\t\t\t {}<<<{}, {}, {}>>>({}); \n\t\t}}\n".format(
+                call_str = "\t\telse if ({} <= {}) {{\n\t\t\t {}<<<{}, {}, {}, stream>>>({}); \n\t\t}}\n".format(
                     symbolic,
                     range_str,
                     function_name,
@@ -421,8 +424,9 @@ extern "C" void init() {{
                     call_args,
                 )
             if last_range == num_items - 1:
-                call_str += ("\t\telse {{\n\t\t\t {}<<<{}, {}, {}>>>({}); \n\t\t}}\n".format(
-                    function_name, grid_str, block_str, smem_str, call_args))
+                call_str += (
+                    "\t\telse {{\n\t\t\t {}<<<{}, {}, {}, stream>>>({}); \n\t\t}}\n".format(
+                        function_name, grid_str, block_str, smem_str, call_args))
             last_range += 1
             _call_str += call_str
 
