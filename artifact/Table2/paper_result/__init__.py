143b097e290af52e9a3f01341114ad8584e42cb9
LeiWang <leiwang1999@outlook.com>
2024-04-24 10:55:21 -0400

    Fix the correctness of Table2

--- artifact/Table2/paper_result/__init__.py
+++ artifact/Table2/paper_result/__init__.py
@@ -2,12 +2,12 @@
 # Licensed under the MIT License.
 compilation_cost = {
     "ResNet(1)": {"AMOS": "3852", "TensorIR": "156", "Welder": "11", "LADDER": "31"},
-    "ResNet(128)": {"AMOS": "3328", "TensorIR": "128", "Welder": "13", "LADDER": "17"},
-    "ShuffleNet(1)": {
-        "AMOS": "2191",
+    "ResNet(128)": {"AMOS": "2191",
         "TensorIR": "836",
         "Welder": "18",
-        "LADDER": "44",
+        "LADDER": "44",},
+    "ShuffleNet(1)": {
+        "AMOS": "3328", "TensorIR": "128", "Welder": "13", "LADDER": "17"
     },
     "ShuffleNet(128)": {
         "AMOS": "3121",
