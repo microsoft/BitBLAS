5c2b8a4b1ced6909e63b71fff86c8d9aca8f95e7
LeiWang1999 <leiwang1999@outlook.com>
2024-05-05 10:10:45 +0000

    chore: Update TVM_HOME path in benchmark_tensorir.sh

--- artifact/Figure8/tensorir-benchmark/benchmark_tensorir.sh
+++ artifact/Figure8/tensorir-benchmark/benchmark_tensorir.sh
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-export TVM_HOME=$(pwd)/../../../baseline_framework/tvm_v0.14.0
+export TVM_HOME=$(pwd)/../../baseline_framework/tvm_v0.14.0
 export PYTHONPATH=$TVM_HOME/python
 pip install xgboost==1.7.1
 
