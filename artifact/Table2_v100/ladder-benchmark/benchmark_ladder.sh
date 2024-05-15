4e401ff414fcf3b8e7d762e24f0be2922f549380
LeiWang1999 <leiwang1999@Ouylook.com>
2024-05-15 05:03:36 +0000

    Implement V100 Table2

---
+++ artifact/Table2_v100/ladder-benchmark/benchmark_ladder.sh
@@ -0,0 +1,66 @@
+# !/bin/bash
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+
+# LADDER_HOME = pwd home + .. + .. + .. + ..
+
+export LADDER_HOME=$(pwd)/../../..
+export LADDER_TVM_HOME=$LADDER_HOME/3rdparty/tvm
+export LADDER_CUTLASS_HOME=$LADDER_HOME/3rdparty/cutlass
+export PYTHONPATH=$LADDER_HOME/python
+export PYTHONPATH=$LADDER_TVM_HOME/python:$PYTHONPATH
+export CPLUS_INCLUDE_PATH=$LADDER_CUTLASS_HOME/include
+
+force_tune=0
+if [[ "$1" == "--force_tune" ]]; then
+    force_tune=1
+fi
+
+echo "[LADDER] Using checkpoint path: $CHECKPOINT_PATH"
+LADDER_LOG_PATH="$CHECKPOINT_PATH/ladder/logs"
+
+if [ -d "$LADDER_LOG_PATH" ] && [ $force_tune -eq 0 ]; then
+    echo "[LADDER] Log directory logs already exists in checkpoints directory. Copying to current directory..."
+    # if the log directory already exists in current directory, remove it
+    if [ -d "./logs" ]; then
+        rm -r "./logs"
+    fi
+    cp "$LADDER_LOG_PATH" "./logs" -r
+fi
+
+# if not force_tune, skip tuning
+
+mkdir -p logs
+
+echo "[LADDER] Running benchmark..."
+
+# Function to run benchmark if needed
+run_benchmark() {
+    local script=$2
+    local options=$3
+    if [ $force_tune -eq 0 ]; then
+        if [ -f "./logs/$1" ]; then
+            echo "[LADDER] Log file $1 already exists, skip tuning ..."
+        else
+            echo "[LADDER] Log file $1 do not exist, start tuning ..."
+            python -u $script $options 2>&1 | tee ./logs/$1
+        fi  
+    else
+        echo "[LADDER] Force tuning ..."
+        python -u $script $options 2>&1 | tee ./logs/$1
+    fi
+}
+
+
+# resnet-50-batch-1 default tuning
+run_benchmark "resnet-50-b1.log" "dequantize_tune_from_onnx.py" "--prefix resnet-50-b1"
+
+# resnet-50-batch-128 default tuning
+run_benchmark "resnet-50-b128.log" "dequantize_tune_from_onnx.py" "--prefix resnet-50-b128"
+
+# shufflenet-batch-1 default tuning
+run_benchmark "shufflenet-b1.log" "dequantize_tune_from_onnx.py" "--prefix shufflenet-b1"
+
+# shufflenet-batch-128 default tuning
+run_benchmark "shufflenet-b128.log" "dequantize_tune_from_onnx.py" "--prefix shufflenet-b128"
---
+++ artifact/Table2_v100/reproduce_result/__init__.py
@@ -0,0 +1,33 @@
+
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+compilation_cost = {
+    "ResNet(1)": {
+        "AMOS": 3851.8527910788853,
+        "TensorIR": 363.70478195211166,
+        "Welder": 8.933333333333334,
+        "LADDER": 31.303672083218892,
+        "Ladder": 31.303672083218892
+    },
+    "ResNet(128)": {
+        "AMOS": 2191.00864,
+        "TensorIR": 836.5209984898568,
+        "Welder": 21.2,
+        "LADDER": 16.819486963748933,
+        "Ladder": 16.819486963748933
+    },
+    "ShuffleNet(1)": {
+        "AMOS": 3327.5927178343136,
+        "TensorIR": 333.765473177035,
+        "Welder": 10.5,
+        "LADDER": 16.695522920290628,
+        "Ladder": 16.695522920290628
+    },
+    "ShuffleNet(128)": {
+        "AMOS": 3121.4316556612653,
+        "TensorIR": 400.51856781244277,
+        "Welder": 12.166666666666666,
+        "LADDER": 37.71550628741582,
+        "Ladder": 37.71550628741582
+    }
+}
