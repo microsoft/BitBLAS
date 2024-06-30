2e5255267d6c71a4701d1b1e7699788e838d26c5
Lei Wang <34334180+LeiWang1999@users.noreply.github.com>
2024-06-30 19:38:35 +0800

    Update target_detector.py

--- python/bitblas/utils/target_detector.py
+++ python/bitblas/utils/target_detector.py
@@ -44,7 +44,7 @@ def get_gpu_model_from_nvidia_smi(gpu_id: int = 0):
 
     # for multiple gpus, CUDA_DEVICE_ORDER=PCI_BUS_ID must be set to match nvidia-smi or else wrong
     # gpu is returned for gpu_id
-    if len(gpus) > 0 and os.environ.get("CUDA_DEVICE_ORDER") != "PCI_BUS_ID":
+    if len(gpus) > 1 and os.environ.get("CUDA_DEVICE_ORDER") != "PCI_BUS_ID":
         raise EnvironmentError("Multi-gpu environment must set `CUDA_DEVICE_ORDER=PCI_BUS_ID`.")
 
     if gpu_id >= len(gpus) or gpu_id < 0:
