f4911c8658acf766c31a7ccdb3f15b57f0b71274
LeiWang1999 <leiwang1999@outlook.com>
2024-07-04 05:32:43 +0000

    Refactor test_ladder_permutate_ops.py for improved readability and maintainability

--- testing/python/operators/test_ladder_permutate_ops.py
+++ testing/python/operators/test_ladder_permutate_ops.py
@@ -9,16 +9,7 @@ target = tvm.target.Target("llvm")
 
 
 # fmt: off
-@pytest.mark.parametrize(
-    "M,N,datatype,dequantize_bits,storage_dtype,propagate_kind,transpose_matrix,transform_kind,target_instruction",
-    [
-        (1024, 1024, "float16", -1, "float16", "B", True, 0, "nvidia-mma"),
-        (1024, 1024, "float16", -1, "float16", "B", True, 1, "nvidia-mma"),
-        (1024, 1024, "float16", -1, "float16", "B", True, 2, "nvidia-mma"),
-        # dequantize propagation
-        (1024, 1024, "float16", 4, "uint32", "B", True, 2, "nvidia-mma"),
-    ])
-def test_ladder_permutate_profile_latency(
+def ladder_permutate_profile_latency(
     M,
     N,
     datatype,
@@ -49,16 +40,13 @@ def test_ladder_permutate_profile_latency(
     assert latency
 
 
-@pytest.mark.parametrize(
-    "M,N,datatype,dequantize_bits,storage_dtype,propagate_kind,transpose_matrix,transform_kind,target_instruction",
-    [
-        (1024, 1024, "float16", -1, "float16", "A", True, 0, "nvidia-mma"),
-        (1024, 1024, "float16", -1, "float16", "A", True, 1, "nvidia-mma"),
-        (1024, 1024, "float16", -1, "float16", "A", True, 2, "nvidia-mma"),
-        # dequantize propagation
-        (1024, 1024, "float16", 4, "uint32", "A", True, 2, "nvidia-mma"),
-    ])
-def test_ladder_permutate_profile_latency_cuda(
+def test_ladder_permutate_profile_latency():
+    ladder_permutate_profile_latency(1024, 1024, "float16", -1, "float16", "B", True, 1, "nvidia-mma")
+    ladder_permutate_profile_latency(1024, 1024, "float16", -1, "float16", "B", True, 2, "nvidia-mma")
+    ladder_permutate_profile_latency(1024, 1024, "float16", 4, "uint32", "B", True, 2, "nvidia-mma")
+
+
+def ladder_permutate_profile_latency_cuda(
     M,
     N,
     datatype,
@@ -91,6 +79,10 @@ def test_ladder_permutate_profile_latency_cuda(
     assert latency
 
 
+def test_ladder_permutate_profile_latency_cuda():
+    ladder_permutate_profile_latency_cuda(1024, 1024, "float16", -1, "float16", "A", True, 1, "nvidia-mma")
+    ladder_permutate_profile_latency_cuda(1024, 1024, "float16", -1, "float16", "A", True, 2, "nvidia-mma")
+    ladder_permutate_profile_latency_cuda(1024, 1024, "float16", 4, "uint32", "A", True, 2, "nvidia-mma")
 # fmt: on
 
 if __name__ == "__main__":
