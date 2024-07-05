5391d1267daa7dca76d9f0f941829bb2e8bf1100
LeiWang1999 <leiwang1999@outlook.com>
2024-07-05 08:31:20 +0000

    Refactor import statements for improved readability and maintainability

--- bitblas/relax/__init__.py
+++ bitblas/relax/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-from .transform import AnnotateDecodeInformation, WeightOnlyLayoutPropagation # noqa: F401
-from .op import tir_interleave_weight # noqa: F401
+from .transform import AnnotateDecodeInformation, WeightOnlyLayoutPropagation  # noqa: F401
+from .op import tir_interleave_weight  # noqa: F401
