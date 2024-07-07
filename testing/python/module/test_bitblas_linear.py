086d208fd07984c8cf876f1529fc80ade4cff21d
LeiWang1999 <leiwang1999@outlook.com>
2024-07-07 16:54:51 +0000

    Refactor BitBLASLinear test module for improved readability and maintainability

--- testing/python/module/test_bitblas_linear.py
+++ testing/python/module/test_bitblas_linear.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 torch.manual_seed(0)
 bitblas.set_log_level("DEBUG")
 
+
 def correctness_consistent(m, in_features, out_features, bias):
     linear_torch = (nn.Linear(in_features, out_features, bias=bias).to(torch.float16).cuda())
     linear_bitblas = BitBLASLinear(
@@ -43,6 +44,7 @@ def test_correctness_consistent():
     correctness_consistent(1, 1024, 1024, True)
     correctness_consistent(1024, 1024, 1024, True)
     correctness_consistent([1, 1024], 1024, 1024, True)
+
 
 def correctness_weight_only_dequantize(
     m,
