64db0655683342ede824c4ca95d0e448479e2e5c
LeiWang1999 <leiwang1999@outlook.com>
2024-09-28 08:11:27 +0000

    lint fix

--- testing/python/operators/test_general_matmul_tilelang_scheduler.py
+++ testing/python/operators/test_general_matmul_tilelang_scheduler.py
@@ -3,20 +3,19 @@
 
 from bitblas import tvm as tvm
 import bitblas.testing
-from tvm import tl
 from tvm.ir import structural_equal
 from bitblas.ops.general_matmul.tilelang.dense.matmul import (
-    MatmulScheduler,
-)
+    MatmulScheduler,)
 
-def test_scheduler_simplify(M,
-                            N,
-                            K,
-                            trans_A=False,
-                            trans_B=True,
-                            dtypeAB="float16",
-                            dtypeC="float16",
-                            accum_dtype="float16"):
+
+def assert_scheduler_simplify(M,
+                              N,
+                              K,
+                              trans_A=False,
+                              trans_B=True,
+                              dtypeAB="float16",
+                              dtypeC="float16",
+                              accum_dtype="float16"):
     matmul = MatmulScheduler(
         M=M,
         N=N,
@@ -27,12 +26,16 @@ def test_scheduler_simplify(M,
         dtypeC=dtypeC,
         accum_dtype=accum_dtype,
     ).disable_simplify().with_default_config()
-    
+
     simplified = MatmulScheduler.Simplify(matmul)
 
     is_equal = structural_equal(matmul, simplified)
-    
-    assert is_equal == False, "Simplify should not return the same schedule"
+    assert is_equal is False, "Simplify should not return the same schedule"
+
+
+def test_scheduler_simplify():
+    assert_scheduler_simplify(128, 128, 128)
+
 
 if __name__ == "__main__":
     bitblas.testing.main()
