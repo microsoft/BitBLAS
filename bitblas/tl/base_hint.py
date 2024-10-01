9e3133624a132e437a1332698b1f64b0701372ff
LeiWang1999 <leiwang1999@outlook.com>
2024-10-01 16:29:30 +0000

    lint fix

--- bitblas/tl/base_hint.py
+++ bitblas/tl/base_hint.py
@@ -2,21 +2,19 @@
 # Licensed under the MIT License.
 from bitblas.base.roller.hint import Hint
 from abc import ABC, abstractmethod
+
+
 class BaseTLHint(ABC):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def __repr__(self):
-        raise NotImplementedError(
-            f"__repr__ is not implemented"
-        )
-    
+        raise NotImplementedError("method __repr__ is not implemented")
+
     def from_roller_hint(self, hint: Hint):
-        raise NotImplementedError(
-            f"from_roller_hint is not implemented"
-        )
-    
+        raise NotImplementedError(f"method from_roller_hint is not implemented")
+
     @abstractmethod
     def get_config_params(self):
         pass
-        
