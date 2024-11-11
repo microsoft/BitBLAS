cde2d3bdaac96fc2c9df3431cebe10c01f262a98
Lei Wang <34334180+LeiWang1999@users.noreply.github.com>
2024-11-11 17:31:15 +0800

    [Dev] Fix illegal pass order (#243)
    * relax transform update
    
    * End2end Fix
    
    * lint fix
    
    ---------
    
    Co-authored-by: LeiWang1999 <leiwaang1999@Outlook.com>

--- bitblas/__init__.py
+++ bitblas/__init__.py
@@ -144,7 +144,10 @@ from .base import (
     try_inline,  # noqa: F401
     try_inline_contiguous_spatial,  # noqa: F401
 )
-
+from .relax import (
+    ApplyDefaultSchedule,  # noqa: F401
+    ApplyFastTuning,  # noqa: F401
+)
 from . import testing  # noqa: F401
 from .utils import auto_detect_nvidia_target, apply_transform_on_input  # noqa: F401
 from .ops.general_matmul import MatmulConfig, Matmul  # noqa: F401
