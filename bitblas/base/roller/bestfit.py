9e0551c64844d532224bb238190e6bd7902d6d68
Your Name <you@example.com>
2025-05-04 23:55:04 +0800

    优化 BestFit 类的 malloc 方法中的条件判断逻辑，修复逻辑错误

--- bitblas/base/roller/bestfit.py
+++ bitblas/base/roller/bestfit.py
@@ -33,7 +33,7 @@ class BestFit:
         size = (size + self.align - 1) // self.align * self.align
         found = None
         for block in self.list:
-            if block.is_free and block.size() >= size and not found or found.size() > block.size():
+            if block.is_free and block.size() >= size and (not found or found.size() > block.size()):
                     found = block
         if found:
             found.is_free = False
